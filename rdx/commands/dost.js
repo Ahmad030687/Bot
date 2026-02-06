78d66e69523f844e0f7a101832ecafde5c0eed27
Ahmad030687 <ahmadalisafdar86@gmail.com>
2026-02-06 23:28:33 +0500

    Update dost.js

--- rdx/commands/dost.js
+++ rdx/commands/dost.js
@@ -1,59 +1,61 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
+const FormData = require("form-data");
 
 module.exports = {
   config: {
     name: "dost",
-    aliases: ["friendship"],
-    credits: "SARDAR RDX",
-    category: "Graphics",
     prefix: true
   },
 
   async run({ api, event }) {
-    const { threadID, messageID, senderID, mentions, messageReply } = event;
+    const { senderID, mentions, messageReply, threadID, messageID } = event;
 
     let uid1 = senderID;
-    let uid2;
-
-    if (Object.keys(mentions).length > 0) {
-      uid2 = Object.keys(mentions)[0];
-    } else if (messageReply) {
-      uid2 = messageReply.senderID;
-    } else {
-      return api.sendMessage("🚫 Dost ko mention ya reply karo!", threadID, messageID);
-    }
-
-    const info = await api.getUserInfo([uid1, uid2]);
-    const name1 = info[uid1]?.name || "Friend 1";
-    const name2 = info[uid2]?.name || "Friend 2";
-
-    const token = "6628568379|c1e620fa708a1d5696fb991c1bde5662";
-    const pfp1 = `https://graph.facebook.com/${uid1}/picture?type=large&access_token=${token}`;
-    const pfp2 = `https://graph.facebook.com/${uid2}/picture?type=large&access_token=${token}`;
-
-    const apiURL =
-      `https://ytdownload-8wpk.onrender.com/api/dost` +
-      `?u1=${encodeURIComponent(pfp1)}` +
-      `&u2=${encodeURIComponent(pfp2)}` +
-      `&n1=${encodeURIComponent(name1)}` +
-      `&n2=${encodeURIComponent(name2)}`;
+    let uid2 = Object.keys(mentions)[0] || messageReply?.senderID;
+    if (!uid2)
+      return api.sendMessage("❌ Dost ko mention ya reply karo", threadID, messageID);
 
     const cache = path.join(__dirname, "cache");
     fs.ensureDirSync(cache);
 
-    const imgPath = path.join(cache, `dost_${uid1}_${uid2}.png`);
-    const res = await axios.get(apiURL, { responseType: "arraybuffer" });
-    fs.writeFileSync(imgPath, res.data);
+    const token = "6628568379|c1e620fa708a1d5696fb991c1bde5662";
 
-    await api.sendMessage({
-      body: `🦅 DOSTI FRAME\n❤️ ${name1} & ${name2}`,
-      attachment: fs.createReadStream(imgPath)
-    }, threadID, messageID);
+    const p1 = path.join(cache, `1_${uid1}.jpg`);
+    const p2 = path.join(cache, `2_${uid2}.jpg`);
 
-    setTimeout(() => {
-      try { fs.unlinkSync(imgPath); } catch {}
-    }, 5000);
+    const download = async (uid, p) => {
+      const url = `https://graph.facebook.com/${uid}/picture?type=large&access_token=${token}`;
+      const res = await axios.get(url, { responseType: "arraybuffer" });
+      fs.writeFileSync(p, res.data);
+    };
+
+    await download(uid1, p1);
+    await download(uid2, p2);
+
+    const info = await api.getUserInfo([uid1, uid2]);
+
+    const form = new FormData();
+    form.append("u1", fs.createReadStream(p1));
+    form.append("u2", fs.createReadStream(p2));
+    form.append("name1", info[uid1].name);
+    form.append("name2", info[uid2].name);
+
+    const res = await axios.post(
+      "https://ytdownload-8wpk.onrender.com/api/dost",
+      form,
+      { headers: form.getHeaders(), responseType: "arraybuffer" }
+    );
+
+    const out = path.join(cache, `final_${Date.now()}.png`);
+    fs.writeFileSync(out, res.data);
+
+    api.sendMessage(
+      { attachment: fs.createReadStream(out) },
+      threadID,
+      () => fs.unlinkSync(out),
+      messageID
+    );
   }
 };

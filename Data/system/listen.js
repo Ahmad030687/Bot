4d52d3620c29c0a520efda9741dddd6dbf5c778c
Ahmad030687 <ahmadalisafdar86@gmail.com>
2026-02-07 22:40:16 +0500

    Update listen.js

--- Data/system/listen.js
+++ Data/system/listen.js
@@ -1,3 +1,30 @@
+"use strict";
+
+var utils = require("../utils");
+
+// @NethWs3Dev - Typing Function Definition
+module.exports = function (defaultFuncs, api, ctx) {
+	return async function sendTypingIndicatorV2(sendTyping, threadID, callback) {
+		let count_req = 0;
+		var wsContent = {
+			app_id: 2220391788200892,
+			payload: JSON.stringify({
+				label: 3,
+				payload: JSON.stringify({
+					thread_key: threadID.toString(),
+					is_group_thread: +(threadID.toString().length >= 16),
+					is_typing: +sendTyping,
+					attribution: 0
+				}),
+				version: 5849951561777440
+			}),
+			request_id: ++count_req,
+			type: 4
+		};
+		await new Promise((resolve, reject) => ctx.mqttClient.publish('/ls_req', JSON.stringify(wsContent), {}, (err, _packet) => err ? reject(err) : resolve()));
+	};
+};
+
 const handleCommand = require('./handle/handleCommand');
 const handleEvent = require('./handle/handleEvent');
 const handleReaction = require('./handle/handleReaction');
@@ -30,6 +57,13 @@ function listen({ api, client, Users, Threads, Currencies, config }) {
       switch (event.type) {
         case 'message':
         case 'message_reply':
+          
+          // 🔥 FIX: Typing Indicator Trigger
+          // Jab bhi koi message aye, bot typing show karega agar woh command ho sakti hai
+          if (event.body && event.body.startsWith(config.PREFIX)) {
+            api.sendTypingIndicator(true, event.threadID);
+          }
+
           if (resendModule && resendModule.logMessage) {
             try {
               const botID = api.getCurrentUserID();

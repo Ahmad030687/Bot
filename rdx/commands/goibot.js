b072f3f78259cf153afbfd262514132e0b27ce42
Ahmad030687 <ahmadalisafdar86@gmail.com>
2026-02-17 12:46:42 +0500

    Update goibot.js

--- rdx/commands/goibot.js
+++ rdx/commands/goibot.js
@@ -730,7 +730,7 @@ async function getAIResponse(userMessage, chatHistory, userName, userGender, sen
       CEREBRAS_API_URL,
       {
         messages: messages,
-        model: "llama-3.3-70b",
+        model: "llama3.3-70b",
         max_completion_tokens: 150,
         temperature: 0.9,
         top_p: 0.95,

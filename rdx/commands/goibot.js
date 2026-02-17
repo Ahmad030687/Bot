082dc1017ef7a0d03e0e5b25caa66a83f2c3e6a6
Ahmad030687 <ahmadalisafdar86@gmail.com>
2026-02-17 21:49:46 +0500

    Update and rename goibot (5).js to goibot.js

--- rdx/commands/goibot.js
+++ rdx/commands/goibot.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const CEREBRAS_API_URL = 'https://api.cerebras.ai/v1/chat/completions';
 
-const API_KEYS = ['ADD YOUR API KEYS HERE - GET FROM https://cerebras.ai./'];
+const API_KEYS = ['csk-ff2wy3ky9xcfh35c3jk22648rykwhje2x6jemd5ptk6jrrdt'];
 
 // Boy Owners (Malik)
 const BOY_OWNERS = {
